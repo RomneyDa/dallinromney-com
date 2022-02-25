0a5b7e5ab20da76aaeae0da94b9a2818bf7cdca3
Dallin Romney <dallinromney@gmail.com>
2022-02-25 02:07:56 -0700

    server

--- server.js
+++ server.js
@@ -20,10 +20,16 @@ const app = express();
 //app.use('/api/users', require('./routes/users'));
 //app.use('/api/auth', require('./routes/auth'));
 //app.use('/api/contacts', require('./routes/contacts'));
+app.use(express.static('client/build'));
 
+
+//make way for some custom css, js and images
+//app.use('/css', express.static(__dirname + '/public/css'));
+//app.use('/js', express.static(__dirname + '/public/js'));
+//app.use('/images', express.static(__dirname + '/public/images'));
 
 // listening port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // start server
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
